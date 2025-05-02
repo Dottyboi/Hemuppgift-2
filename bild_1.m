5e4efc23ed7a36283c7108f994b68738eadb1b6d
Dottyboi <Fabian.pizarro.04@gmail.com>
2025-05-02 23:00:05 +0200

    fungerande kod
    test visar spektrumet för insignalen

---
+++ bild_1.m
@@ -0,0 +1,11 @@
+z=mkhwdata([040622;010326]);
+
+[Z,nu]=tdftfast(z);
+
+[b, a] = butter(5, 0.2, "low");
+
+freqz(b,a,nu);
+
+y = filter(b,a,z);
+
+present_image(y);
---
+++ bild_3.m
@@ -0,0 +1,17 @@
+z=mkhwdata([040622;010326]);
+
+[Z,nu]=tdftfast(z);
+
+n = 0:(length(z) - 1);
+
+mult  = cos(0.5*pi.*n);
+
+[z1] =  (z.*transpose(mult)); 
+
+[b, a] = butter(5, 0.3, "low");
+
+freqz(b,a,nu,"whole",0.5);
+
+y = filter(b,a,z1);
+
+present_image(y);
